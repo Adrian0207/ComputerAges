67119627d2e501792b624ad38715d63f117885c7
Adrian Oliva <adrian69oliva@gmail.com>
2020-08-16 12:44:53 -0500

    Gears move
    add script Gear Rotate to the gears

--- Assets/Scripts/GearRotate.cs
+++ Assets/Scripts/GearRotate.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class GearRotate : MonoBehaviour
 {
     public float rotateSpeed = 1f;
+    public int direction = -1;
     void FixedUpdate()
     {
-        transform.Rotate(Vector3.forward * -rotateSpeed);     
+        transform.Rotate(Vector3.forward * (rotateSpeed* direction));     
     }
 }
